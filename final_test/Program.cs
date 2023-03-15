cd86a85f5dda8d658a407103250583ce55a60365
Yan San Din <yan_san_din@mail.ru>
2023-03-15 10:16:50 +0300

    added threeCharArray

--- final_test/Program.cs
+++ final_test/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] UserInputArray(int num)
+{
+    string[] array = new string[num];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите строку №{i}: ");
+        array[i] = Console.ReadLine()!;
+    }
+    return array;
+}
+
+string[] ThreeCharArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+            count++;
+    }
+    string[] FinalArray = new string[count];
+    count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            FinalArray[count] = array[i];
+            count++;
+        }
+    }    
+    return FinalArray;
+}
